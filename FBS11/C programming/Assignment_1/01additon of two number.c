f6e5fa44b3f72975a723e977f320d56fa3b61520
Pruthviraj-Walkunde <walkundepruthviraj@gmail.com>
2026-08-06 22:22:34 +0530

    Add files via upload

---
+++ FBS11/C programming/Assignment_1/01additon of two number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+int a,b,sum;
+	a=10;
+	b=20;
+	sum=a+b;
+	printf("sum =%d",sum);
+	
+}
